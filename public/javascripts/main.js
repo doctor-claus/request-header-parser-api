4eed3860bb0e66e2f2cf573a88dc519b688925cf
doctor-claus <deysouvik107@gmail.com>
2018-05-21 16:03:40 +0530

    Hostname edits

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $.getJSON("http://localhost:3000/browserinfo", (result) => {
+    $.getJSON("https://req-head-parser-api.herokuapp.com/browserinfo", (result) => {
         $("#ip").text(result.ip);
         $("#lang").text(result.language);
         $("#soft").text(result.software);
